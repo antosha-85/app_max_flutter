454878fd3693198837c57a7bb9b48b45df41bbc7
Anton Smirnov <antosha-85@yandex.ru>
2021-05-07 15:18:28 -0600

    Created one method and three buttons and then created a stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,54 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
+    ];
+    void answerQuestion() {
+      setState(() {
+        questionIndex++;
+      });
+      print('question Index ' + questionIndex.toString());
+    }
+
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Anton's App"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(questions[questionIndex]),
+            ElevatedButton(
+              child: Text(questions[0]),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 chosen!'),
+            ),
+            ElevatedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                //
+                print('Answer 3 chosen!');
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
